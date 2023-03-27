8b4a930a0b6227e6585bffc9cfb7e5ccad6b2cfb
Yacoub14 <oualijaneyacoub@gmail.com>
2023-03-27 16:57:53 -0700

    pointers arrays strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,14 +7,19 @@
  */
 void puts_half(char *str)
 {
-int length = 0, i, n;
-while (str[length] != '\0')
-length++;
-if (length % 2 == 0)
-n = length / 2;
-else
-n = (length - 1) / 2;
-for (i = n; i < length; i++)
-_putchar(str[i]);
-_putchar('\n');
+	int i, n, len;
+
+	len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+
+	n = (len / 2);
+
+	if ((len % 2) == 1)
+		n = ((len + 1) / 2);
+
+	for (i = n; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }
